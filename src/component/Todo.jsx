e925eb95b2d220f4041b994fde3533b20d604f57
KirtiJha <kirti.jha2025@gmail.com>
2025-03-31 16:27:03 +0530

    added hook-useSelector

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux"
+
 export default function Todo(){
+    const todos = useSelector((state) => state.todos);
+    console.log(todos);
+
     return(
         <>
-        <h2>Todos</h2>
+        <h2>Todo List App</h2>
+        {/* printing todo in ui */}
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.task}</li>
+                ))}
+        </ul>
         </>
-    )
+    );
 }
